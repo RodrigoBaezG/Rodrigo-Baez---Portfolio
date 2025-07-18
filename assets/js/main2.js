aaae30d13d869984d6f114cbd3d152bf34f1d2e9
Kbaez <kbaez.dibujos@gmail.com>
2025-07-18 12:48:55 +0100

    first commit

---
+++ assets/js/main2.js
@@ -0,0 +1,8 @@
+(function () {
+    const nav = document.getElementById('navbar');
+    const button = document.getElementById('burger');
+
+    button.addEventListener('click', () => {
+        nav.classList.toggle('open');
+    });
+})();
